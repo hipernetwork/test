2af286887fabe00ab19ffc140bd122ae01b0dafe
Koray Üstündağ <mskorayustundag@gmail.com>
2025-04-07 19:42:36 +0300

    Create Code.cs

---
+++ Code.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
